cc6a5d5005ef100c16154156266f6f92fa67fe1e
nkarl <21993921+nkarl@users.noreply.github.com>
2023-03-10 20:08:48 -0800

    Updated.

--- week-8/hw4DB/CptS451_HW4_CharlesNguyen.sql
+++ week-8/hw4DB/CptS451_HW4_CharlesNguyen.sql
@@ -1,3 +1,6 @@
+-- Name: Charles Nguyen
+-- WSU ID: 011606177
+
 -- 1
 SELECT  *
 FROM
@@ -122,7 +125,11 @@ WHERE
 -- Find the courses given in the ‘Sloan’ building, which
 --      have enrolled more students than their enrollment limit.
 -- Return the courseno, enroll_limit, and the actual enrollment for those courses.
-SELECT C2.courseno, C3.enroll_limit, C2.enrollnum FROM (
+SELECT
+    C2.courseno,
+    C3.enroll_limit,
+    C2.enrollnum
+FROM (
     SELECT
         C1.courseno,
         COUNT(C1.sid) AS enrollnum
@@ -139,4 +146,75 @@ SELECT C2.courseno, C3.enroll_limit, C2.enrollnum FROM (
     JOIN course C3
     ON C3.courseno=C2.courseno
     WHERE C2.enrollnum>C3.enroll_limit;
+SELECT *
+FROM (
+    SELECT DISTINCT
+        C1.courseno, C1.enroll_limit, 
+        COUNT(C1.sid)
+    OVER (PARTITION BY C1.courseno)
+    FROM (
+        SELECT DISTINCT
+            C.courseno,
+            C.enroll_limit,
+            E.sid
+        FROM
+            course AS C,
+            enroll AS E
+        WHERE
+            classroom LIKE 'Sloan%'
+            AND C.courseno=E.courseno) AS C1) AS C2
+WHERE C2.enroll_limit<C2.count;
 
+
+-- 7
+SELECT
+    LOW.sid,
+    S.sname,
+    LOW.courseno,
+    LOW.grade
+FROM
+    Student S,
+    (SELECT DISTINCT
+        PR.sid, P.courseno, PR.precourseno, PR.grade
+        FROM    Prereq P,
+                (SELECT DISTINCT
+                    enroll.sid, prereq.precourseno, enroll.grade
+                    FROM enroll, prereq
+                    WHERE enroll.courseno=prereq.precourseno) AS PR
+        WHERE P.precourseno=PR.precourseno
+        AND PR.grade<2) AS LOW
+WHERE
+    S.sid=LOW.sid
+ORDER BY
+    S.sname, LOW.courseno
+;
+
+-- 8
+SELECT DISTINCT
+    *,
+    passing*100/total AS rate
+FROM    (SELECT courseno, count(*) AS passing
+            FROM enroll WHERE grade>=2 GROUP BY courseno) AS Passed,
+        (SELECT courseno, COUNT(*) AS Total
+            FROM enroll WHERE courseno LIKE 'CptS%'
+            GROUP BY courseno) AS Rolls
+WHERE
+    Passed.courseno=Rolls.courseno
+    -- AND Rolls.courseno LIKE "CptS%"
+ORDER BY Rolls.courseno
+;
+
+-- 9
+-- 1. equi join Course and Prereq by courseno,
+-- 2. aggregate by counting by precourseno,
+-- 3. select the courseno where pcount >= 2
+-- 4. order by courseno
+SELECT *
+FROM (SELECT
+        C.courseno, COUNT(P.precourseno) AS pcount
+        FROM course C, prereq P
+        WHERE C.courseno=P.courseno
+        GROUP BY C.courseno) AS C1
+WHERE pcount >= 2
+ORDER BY courseno
+;
